2e531465433af031e3b31ba7ddc348b43953825d
Phùng Huy Quang <ITITWE22145@student.hcmiu.edu.vn>
2024-12-26 11:41:43 +0700

    re

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'your_secret_key'  # Change this to a random secret key
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bus_reservation.db'  # Change to your database
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
